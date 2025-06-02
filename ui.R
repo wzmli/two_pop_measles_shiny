22d4e89e87addc90c41b2c9ffcbd6419e93702d8
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-06-02 15:30:28 -0400

    flex layout

--- ui.R
+++ ui.R
@@ -4,98 +4,44 @@ theme_set(theme_bw(base_size=20))
 library(dplyr)
 library(macpan2)
 
-# Define UI for application that draws a histogram
 fluidPage(
-	# Application title
-	titlePanel("Two population Measles Model"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-		sidebarPanel(
-		  checkboxInput("det"
-		                ,"Deterministic"
-		                , value = FALSE
-		  )
-		  ,
-		  checkboxInput("stoch"
-		                ,"Stochastic"
-		                , value = TRUE
-		  )
-		  ,
-		  numericInput("nsims"
-		    , "Number of Simulations" 
-		    , value = 10 
-		    )
-		  ,
-		  numericInput("pop_red"
-		               , "Pop Red" 
-		               , value = 1000 
-		  )
-		  ,
-		  numericInput("pop_blue"
-		               , "Pop Blue" 
-		               , value = 1000 
-		  )
-		  ,
-		  sliderInput("Rzero"
-		              ,"Reproduction Number"
-		              , min = 0
-		              , max = 18
-		              , value=12
-		  )
-		  ,
-		  sliderInput("A0"
-		              ,"Awareness"
-		              , min = 0.001
-		              , max = 0.2
-		              , value=0.1
-		  )
-		  ,
-		  sliderInput("pref"
-		    ,"Within Group Mixing"
-		    , min = 0
-		    , max = 1
-		    , value=0.99
-		  ),
-		  sliderInput("eff"
-		              ,"Vaccine Effectiveness"
-		              , min = 0
-		              , max = 1
-		              , value=0.95
-		  )
-		  ,
-      	sliderInput("vaxprop_red"
-         	,"Vaccine Coverage of RED"
-      		, min = 0
-      		, max = 1
-      		, value=0.5
-      		)
-      	,
-      	sliderInput("vaxprop_blue"
-      	            ,"Vaccine Coverage of BLUE"
-      	            , min = 0
-      	            , max = 1
-      	            , value=0.5
-      	),
-		  sliderInput("iso_red"
-		    ,"Infection Isolation of RED"
-		    , min = 0
-		    , max = 1
-		    , value=0.5
-		  )
-		  ,
-		  sliderInput("iso_blue"
-		    ,"Infection Isolation of BLUE"
-		    , min = 0
-		    , max = 1
-		    , value=0.5
-		  )
-		)
-	, mainPanel(
-         plotOutput("incplot",height = 950)
-
-		
+  titlePanel("Two Population Measles Model"),
+  
+  fluidRow(
+    column(6,
+      fluidRow(
+        column(6,
+          h3("Model Settings"),
+          checkboxInput("det", "Deterministic", value = FALSE),
+          checkboxInput("stoch", "Stochastic", value = TRUE),
+          numericInput("nsims", "Number of Simulations", value = 10),
+          sliderInput("Rzero", "Reproduction Number", min = 0, max = 18, value = 12)
+        ),
+        column(6,
+          br(),
+          sliderInput("A0", "Awareness", min = 0.001, max = 0.2, value = 0.1),
+          sliderInput("pref", "Within Group Mixing", min = 0, max = 1, value = 0.99),
+          sliderInput("eff", "Vaccine Effectiveness", min = 0, max = 1, value = 0.95)
+        )
+      ),
+      fluidRow(
+        column(6,
+          h3("Red Controls"),
+          numericInput("pop_red", "Pop Red", value = 1000),
+          sliderInput("vaxprop_red", "Vaccine Coverage of RED", min = 0, max = 1, value = 0.5),
+          sliderInput("iso_red", "Infection Isolation of RED", min = 0, max = 1, value = 0.5)
+        ),
+        column(6,
+          h3("Blue Controls"),
+          numericInput("pop_blue", "Pop Blue", value = 1000),
+          sliderInput("vaxprop_blue", "Vaccine Coverage of BLUE", min = 0, max = 1, value = 0.5),
+          sliderInput("iso_blue", "Infection Isolation of BLUE", min = 0, max = 1, value = 0.5)
+        )
       )
-   )
+    ),
+    
+    column(6,
+      plotOutput("incplot", height = "1000px")
+    )
+  )
 )
-
